a5268117a649bbd6ff4ae2b9d3d002460f13dbef
HashiCorp Training <training@hashicorp.com>
2017-04-12 16:59:46 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,66 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your AMI ID is:
+#
+#     ami-eea9f38e
+#
+# Your subnet ID is:
+#
+#     subnet-7e08481a
+#
+# Your security group ID is:
+#
+#     sg-834d35e4
+#
+# Your Identity is:
+#
+#     autodesk-dinosaur
+#
+
+terraform {
+  backend "atlas" {
+    name = "willhou/training"
+  }
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "region" {
+  default = "us-west-1"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.region}"
+}
+
+variable "num_webs" {
+  default = "2"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-eea9f38e"
+  count         = "${var.num_webs}"
+  instance_type = "t2.micro"
+
+  subnet_id              = "subnet-7e08481a"
+  vpc_security_group_ids = ["sg-834d35e4"]
+
+  tags {
+    identity = "autodesk-dinosaur"
+    Function = "AD"
+    use      = "Prod"
+    Name     = "Web $(count.index+1}/${var.num_webs}"
+  }
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
